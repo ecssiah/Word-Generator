a524051ec707ad30f9eb241bf3e9e07a20a7b333
ecssiah <ecssiah@gmail.com>
2018-10-07 22:58:30 -0500

    Update generation algorithm

--- word_generator.py
+++ word_generator.py
@@ -1,32 +1,45 @@
+# -*- coding: utf-8 -*-
+
 import random
 
-vowels = "o i a e u"
-consonants = "s n c p m r z l v j b d t h f Zh w y ng g"
-diphthongs = "oo oi oa oe ou io ii ia iu eo aa au eo ee eu uo ui ua ue uu"
+vowels = "o oo i ii a aa e ee u uu"
+consonants = "s n c p m r z l v j b d t h f ʒ w y ŋ g"
+vowel_digraphs = "oi oa oe ou io ia iu eo au eo eu uo ui ua ue"
 consonant_digraphs = "ch sh th"
 
 V = vowels.split()
 C = consonants.split()
-D = diphthongs.split()
-G = consonant_digraphs.split()
+VD = vowel_digraphs.split()
+CD = consonant_digraphs.split()
 
-for i in range(100):
-  word = ""
+for i in range(40):
+    word = ""
 
-  if random.randrange(100) < 50:
-    word += random.choice(V)
-  else:
-    word += random.choice(D)
+    if random.randrange(2) == 0:
+        if random.randrange(2) == 0:
+            word += random.choice(VD)
+        else:
+            word += random.choice(V)
 
-  if random.randrange(100) < 50:
-    word += random.choice(C)
+        if random.randrange(10) == 0:
+            word += random.choice(CD)
+        else:
+            word += random.choice(C)
 
-  if random.randrange(100) < 30:
-    word += random.choice(D)
-  else:
-    word += random.choice(C)
+        if random.randrange(2) == 0:
+            word += random.choice(V)
+    else:
+        if random.randrange(2) == 0:
+            word += random.choice(CD)
+        else:
+            word += random.choice(C)
+        
+        if random.randrange(10) == 0:
+            word += random.choice(VD)
+        else:
+            word += random.choice(V)
 
-  if random.randrange(100) < 50:
-    word += random.choice(C)
+        if random.randrange(2) == 0:
+            word += random.choice(C)
 
-  print(word)
+    print(word)
